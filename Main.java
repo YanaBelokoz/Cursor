e531cd26ce8d99ca25c07244f0e5367b7f8a1729
YanaBelokoz <110562388+YanaBelokoz@users.noreply.github.com>
2022-08-07 11:11:08 +0300

    Home Work Update

--- Main.java
+++ Main.java
@@ -1,34 +1,11 @@
 package com.example.helloworld.calculate;
 
-import com.example.helloworld.Calculate;
-import com.example.helloworld.Main;
-
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        String action;
-        Calculate calculate = new Calculate();
-        Scanner input = new Scanner(System.in);
+        CalculatorService calculatorService = new CalculatorService();
+        calculatorService.execute();
     }
 
-    public static void calculate(char action) {
-        default
-        float calculate;
-        float firstNumber, char action, float secondNumber; {
-            float result = 0;
-            switch (action) {
-                Object result;
-                case "+" -> result = firstNumber + secondNumber;
-                case "-" -> result = firstNumber + secondNumber;
-                case "*" -> result = firstNumber * secondNumber;
-                case "/" -> result = firstNumber / secondNumber;
-            }
-            return result;
-        }
-
-    }
 }
 
 
